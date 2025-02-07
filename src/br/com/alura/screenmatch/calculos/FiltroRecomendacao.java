2b354623ae6955b995f0a441f44a067fdf9905dc
Sebastiao de Paula <sebastiao.paula@gmail.com>
2025-02-06 21:32:02 -0300

    utilisar Collections

--- src/br/com/alura/screenmatch/calculos/FiltroRecomendacao.java
+++ src/br/com/alura/screenmatch/calculos/FiltroRecomendacao.java
@@ -3,7 +3,7 @@ package br.com.alura.screenmatch.calculos;
 public class FiltroRecomendacao {
 
     public void filtra(Classificavel classificavel){
-        if (classificavel.getClassificacao() >= 4){
+        if (classificavel. getClassificacao() >= 4){
             System.out.println("Está entre os preferidos do momento");
         }else if(classificavel.getClassificacao() >=2){
             System.out.println("Muito bem avaliado");
